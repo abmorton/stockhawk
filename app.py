8540bbf853f17c6492f332ef0cfebd02cd742db2
abmorton <abmorton@gmail.com>
2015-10-09 22:10:00 -0700

    fixed stock search bug when including invalid characters

--- app.py
+++ app.py
@@ -28,6 +28,57 @@ def login_required(f):
 			return redirect(url_for('login'))
 	return wrap
 
+# helper functions
+
+# This is to output conditional formatting into the stocks.html template.
+def set_color(change):
+	if change < 0.0000000:
+		loss = True
+	else:
+		loss = None
+	return loss
+
+# This is to take out punctuation and white spaces from the stock search string.
+def clean_stock_search(symbol):
+	punctuation = '''!()-[]{ };:'"\,<>./?@#$%^&*_~0123456789'''
+	no_punct = ""
+	for char in symbol:
+		if char not in punctuation:
+			no_punct = no_punct + char
+	if len(no_punct) == 0:
+		no_punct = 'RETRY'
+	return no_punct
+
+# Puts various attributes into 'stock' via different Share methods.
+def set_stock_data(stock):
+	stock.name = stock.data_set["Name"]
+	stock.symbol = stock.data_set["Symbol"].upper()
+	stock.exchange = stock.get_stock_exchange()
+	stock.price = stock.get_price()
+	stock.change = stock.get_change()
+	stock.percent_change = stock.data_set["PercentChange"]
+	stock.afterhours = stock.data_set['AfterHoursChangeRealtime']
+	stock.last_traded = stock.get_trade_datetime()
+	stock.prev_close = stock.get_prev_close()
+	stock.open = stock.get_open()
+	stock.bid = stock.data_set['Bid']
+	stock.ask = stock.data_set['Ask']
+	stock.yr_target = stock.data_set['OneyrTargetPrice']
+	stock.volume = stock.get_volume()
+	stock.av_volume = stock.get_avg_daily_volume()
+	stock.day_low = stock.get_days_low()
+	stock.day_high = stock.get_days_high()
+	stock.day_range = stock.day_high+" - "+stock.day_low
+	stock.year_high = stock.get_year_high()
+	stock.year_low = stock.get_year_low()
+	stock.year_range = stock.year_high+" - "+stock.year_low
+	stock.market_cap = stock.data_set["MarketCapitalization"]
+	stock.peratio = stock.data_set["PERatio"]
+	stock.div = stock.data_set["DividendYield"]
+	stock.ex_div = stock.data_set['ExDividendDate']
+	stock.div_pay = stock.data_set['DividendPayDate']
+	return stock
+
 # views
 
 @app.route('/')
@@ -78,17 +129,10 @@ def stocks():
 	stock = None
 	form = StockSearchForm(request.form)
 
-	def set_color(change):
-		if change < 0.0000000:
-			loss = True
-		else:
-			loss = None
-		return loss
-
 	if request.method == 'POST':
 		if form.validate():
 
-			stock = Share(form.stocklookup.data)
+			stock = Share(clean_stock_search(form.stocklookup.data))
 
 			if stock.get_price() == None:
 				flash("Couldn't find stock matching '"+form.stocklookup.data.upper()+"'. Try another symbol.")
@@ -97,35 +141,8 @@ def stocks():
 			else:
 				change = float(stock.get_change())
 				loss = set_color(change)
-				title = stock.symbol.upper()+" - StockHawk"
-
-				# trying to consolidate stock attributes into one place.
-				stock.name = stock.data_set["Name"]
-				stock.symbol = stock.symbol.upper()
-				stock.exchange = stock.get_stock_exchange()
-				stock.price = stock.get_price()
-				stock.change = stock.get_change()
-				stock.percent_change = stock.data_set["PercentChange"]
-				stock.afterhours = stock.data_set['AfterHoursChangeRealtime']
-				stock.last_traded = stock.get_trade_datetime()
-				stock.prev_close = stock.get_prev_close()
-				stock.open = stock.get_open()
-				stock.bid = stock.data_set['Bid']
-				stock.ask = stock.data_set['Ask']
-				stock.yr_target = stock.data_set['OneyrTargetPrice']
-				stock.volume = stock.get_volume()
-				stock.av_volume = stock.get_avg_daily_volume()
-				stock.day_low = stock.get_days_low()
-				stock.day_high = stock.get_days_high()
-				stock.day_range = stock.day_high+" - "+stock.day_low
-				stock.year_high = stock.get_year_high()
-				stock.year_low = stock.get_year_low()
-				stock.year_range = stock.year_high+" - "+stock.year_low
-				stock.market_cap = stock.data_set["MarketCapitalization"]
-				stock.peratio = stock.data_set["PERatio"]
-				stock.div = stock.data_set["DividendYield"]
-				stock.ex_div = stock.data_set['ExDividendDate']
-				stock.div_pay = stock.data_set['DividendPayDate']
+				stock = set_stock_data(stock)
+				title = stock.symbol+" - StockHawk"
 
 		else:
 			flash("Please enter a stock.")
@@ -135,7 +152,6 @@ def stocks():
 
 	elif request.method == 'GET':
 		return render_template('stocks.html', form=form, stock=stock, title=title)
-
 
 
 if __name__ == '__main__':
