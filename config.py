02d7be70525948ef666173b2a8472442012d33fb
abmorton <abmorton@gmail.com>
2015-10-13 12:46:58 -0700

    updated db_view page

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class BaseConfig(object):
 	DEBUG = False
 	WTF_CSRF_ENABLED = True
-	SECRET_KEY = 'Hvee95a202[a1d'
+	SECRET_KEY = 'Hvee923sdf@#234argag~``gdgsa;5a202[a1d'
 
-# class DevConfig(BaseConfig):
-# 	DEBUG = True
-# 	SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskpractice.db'
+class DevConfig(BaseConfig):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskpractice.db'
 
 class ProductionConfig(BaseConfig):
 	DEBUG = False
