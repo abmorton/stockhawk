e6131b20482e466e558706cbd0bd1a9329cbdd3f
abmorton <abmorton@gmail.com>
2015-10-19 00:30:02 -0700

    removed login_reminder decorator from stocks/<symbol> view function due to duplicate flash message bug

--- app.py
+++ app.py
@@ -502,7 +502,7 @@ def stocks():
 		return render_template('stocks.html', form=form, tradeform=tradeform, stock=stock, stocks=stocks, leaders=leaders, title=title, user=user, loggedin_user=loggedin_user)
 
 @app.route('/stocks/<symbol>', methods=['GET', 'POST'])
-@login_reminder
+# @login_reminder
 def stock(symbol):
 	stock = Share(symbol)
 	if stock.get_price() == None:
