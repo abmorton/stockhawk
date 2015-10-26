e6de786e7a9dce35fda6f6a4c9cec21bfd380107
abmorton <abmorton@gmail.com>
2015-10-26 00:13:01 -0700

    tinkering with config file to get mail to work

--- config.py
+++ config.py
@@ -8,27 +8,24 @@ class BaseConfig(object):
 	# MAIL_USE_SSL = True
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
+	MAIL_SERVER = os.environ['MAIL_SERVER']
+	MAIL_USERNAME = os.environ['MAIL_USERNAME']
+	MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+	MAIL_DEFAULT_SENDER = MAIL_USERNAME
 
 class DevConfig(BaseConfig):
 	DEBUG = True
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskpractice.db'
-	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_USERNAME = os.environ['MAIL_USERNAME']
-	MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
+	# MAIL_SERVER = 'smtp.gmail.com'
 
 	# MAIL_SERVER = 
 	# MAIL_USERNAME = 
 	# MAIL_PASSWORD = 
 
-	MAIL_DEFAULT_SENDER = MAIL_USERNAME
-
 class ProductionConfig(BaseConfig):
 	DEBUG = False
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-	MAIL_SERVER = os.environ['MAILGUN_SMTP_SERVER']
-	MAIL_USERNAME = os.environ['MAILGUN_SMTP_LOGIN']
-	MAIL_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-	MAIL_DEFAULT_SENDER = MAIL_USERNAME
+
 
 
 
