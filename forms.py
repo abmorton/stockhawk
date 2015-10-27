3629f53ccaf12fc199b93b6f744a0d9f0487fcb0
abmorton <abmorton@gmail.com>
2015-10-26 23:40:11 -0700

    added 'register' to navbar dropdown while user is not logged in

--- forms.py
+++ forms.py
@@ -28,7 +28,7 @@ class RegisterForm(Form):
 	email =  TextField('Email address', validators=[Length(min=6, max=50), Email(message='Please enter a valid email address.')])
 	password = PasswordField('Password', validators=[Length(min=6, max=30, message='Password must be between 6 and 30 characters.')])
 	confirm = PasswordField('Confirm password', validators=[EqualTo('password', message='Passwords must match.')])
-	accept_tos = BooleanField('I accept the Terms of Service (required)', validators=[Required(message='You must accept the Terms of Service to register and account.')])
+	accept_tos = BooleanField('I accept the Terms of Service (required)', validators=[Required(message='You must accept the Terms of Service to register an account.')])
 
 class TradeForm(Form):
 	amount = IntegerField('Shares', [validators.NumberRange(min=1, max=9999999999999, message="Invalid share quantity. Please try again.")])
