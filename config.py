8618ace0982a24e9e7d0e01d811ef4903bc8ff79
abmorton <abmorton@gmail.com>
2015-10-26 17:44:01 -0700

    minor css updates for navigation bar

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ class DevConfig(BaseConfig):
 	# MAIL_USERNAME = 
 	# MAIL_PASSWORD = 
 
-	CELERY_BROKER_URL = 'redis://localhost:6379/0'
-	CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+	# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+	# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 class ProductionConfig(BaseConfig):
 	DEBUG = False
